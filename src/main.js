71e14a1640f777566e99aeb257a474ea8fdbe30d
dirstart <q835041058@gmail.com>
2020-04-19 15:02:45 +0800

    引用字体的第二种方法

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './icons';
+import './assets/fonts/daysOne.css';
 
 Vue.config.productionTip = false;
 
